078cb5b3746e8e46d994a927b7685b8ed1cbef5f
yangruichao <yangruichaoyrc@163.com>
2022-01-05 09:38:42 +0800

    feat(superb-ui/button/**): 完善button按钮功能

--- packages/superb-ui/src/button/button.tsx
+++ packages/superb-ui/src/button/button.tsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import classNames from 'classnames'
 import { ButtonProps } from './types'
+import './button.less'
 
 const prefixCls = 'superb-btn'
 
 const Button = function (props: ButtonProps) {
-  const { type, size, className } = props
+  const {
+    type = 'default',
+    text,
+    outline,
+    disabled,
+    round,
+    size = 'normal',
+    block,
+    className,
+  } = props
   const classes = classNames(
     'superb-btn',
     {
-      [`${prefixCls}--${type}`]: true,
+      [`${prefixCls}--${type}`]: type,
       [`${prefixCls}--${size}`]: size,
+      [`${prefixCls}--text`]: text,
+      [`${prefixCls}--text-${type}`]: text,
+      [`${prefixCls}--text-disabled`]: disabled,
+      [`${prefixCls}--outline`]: outline,
+      [`${prefixCls}--block`]: block,
+      [`${prefixCls}--disabled`]: disabled,
+      [`${prefixCls}--round`]: round,
     },
     className
   )
-  return <button className={classes}>{props.children}</button>
+  return (
+    <button className={classes}>
+      <div className="superb-btn--content">{props.children}</div>
+    </button>
+  )
 }
 
 export default Button
